d8acc61534281304a3295599cdf4f1b51f4d695f
guobb <18576469764@163.com>
2016-12-12 09:00:42 +0800

    test
    test demo

---
+++ test.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by apple on 16/12/12.
+ */
+exports.text = 'typescript';
+var Text = (function () {
+    function Text() {
+    }
+    return Text;
+}());
+exports.Text = Text;
---
+++ test.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by apple on 16/12/12.
+ */
+export var text = 'typescript';
+
+export class Text {
+
+}
